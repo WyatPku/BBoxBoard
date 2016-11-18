aa05e5a5b20dc92647670ba3f9e101be3ed591dc
mew <377545660@qq.com>
2016-11-18 08:53:34 +0800

    尝试了把Rectangle和Line放到Shape类型里

---
+++ BBoxBoard/BasicDraw/MyShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace BBoxBoard.BasicDraw
+{
+    class MyShape
+    {
+        MyShape()
+        {
+            Shape shape = new Rectangle();
+            Shape shape2 = new Line();
+        }
+    }
+}
--- BBoxBoard/MainWindow.xaml.cs
+++ BBoxBoard/MainWindow.xaml.cs
@@ -43,15 +43,15 @@ namespace BBoxBoard
             var targetElement = Mouse.Captured as UIElement;
             if (e.LeftButton == MouseButtonState.Pressed && targetElement != null)
             {
-                var pCanvas = e.GetPosition(Mycanvas);
-                Canvas.SetLeft(targetElement, pCanvas.X - targetPoint.X);
-                Canvas.SetTop(targetElement, pCanvas.Y - targetPoint.Y);
+                Point pCanvas = e.GetPosition(Mycanvas);
+                textBox.Text = "X:" + pCanvas.X + "\nY:" + pCanvas.Y;
             }
         }
 
         private void Mycanvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
             Mouse.Capture(null);
+            //取消正在移动的东西，并刷新电路网格
         }
 
         private void Mycanvas_MouseDown(object sender, MouseButtonEventArgs e)
@@ -59,8 +59,13 @@ namespace BBoxBoard
             var targetElement = e.Source as IInputElement;
             if (targetElement != null)
             {
-                targetPoint = e.GetPosition(targetElement);
                 targetElement.CaptureMouse();
+                /*
+                 * 点下的时候判断有没有选中某一个电子元件
+                 * 如果选中了，记录状态，记录初始位置和初始光标位置
+                 * Move的时候一直跟着动，但是位置不是鼠标的位置，而是贴合位置
+                 * 这和第一次放上去元件是一样的，只能放在格点上
+                 */
             }
         }
 
